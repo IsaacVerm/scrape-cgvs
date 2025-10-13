55736bb3faaeae05b71a4e83e924c6c697286190
IsaacVerm <isaacverm@gmail.com>
2025-10-13 12:59:08 +0000

    add workload field

--- scrape.sh
+++ scrape.sh
@@ -6,7 +6,7 @@ MONTHS=("januari" "februari" "maart" "april" "mei" "juni" "juli" "augustus" "sep
 
 # create empty CSV file with just a header if file doesn't exist
 if [ ! -f "$OUTPUT_FILE" ]; then
-    echo "month,year,count_applicants_international_protection,top_10_nationalities_applicants_international_protection,count_cgvs_decisions" > "$OUTPUT_FILE"
+    echo "month,year,count_applicants_international_protection,top_10_nationalities_applicants_international_protection,count_cgvs_decisions,workload" > "$OUTPUT_FILE"
 fi
 
 # fetch page each month, extract required values and output the result to CSV
@@ -35,9 +35,10 @@ for YEAR in {2020..2025}; do
         COUNT_APPLICANTS_INTERNATIONAL_PROTECTION=$(echo "$HTML_CONTENT" | sed -En 's/.*registreerde de DVZ.<strong>([0-9]*(\.[0-9]*)?).*/\1/p')
         TOP_10_NATIONALITIES_APPLICANTS_INTERNATIONAL_PROTECTION=$(echo "$HTML_CONTENT" | sed -En 's/<li>([A-Z].*)staan in .* bovenaan de top 10.*\. (.*)vervolledigen de top 10.*/"\1,\2"/p')
         COUNT_CGVS_DECISIONS=$(echo "$HTML_CONTENT" | sed -En 's/.*<strong>([0-9]*(\.[0-9]*)?)( )?<\/strong>( )?beslissingen.*/\1/p') # https://www.cgvs.be/nl/actueel/asielstatistieken-mei-2024 I wasn't able to match May 2024 data
-                
+        WORKLOAD=$(echo "$HTML_CONTENT" | sed -En 's/.*bedroeg de totale werklast <strong>([0-9]*(\.[0-9]*)?).*/\1/p')
+
         # save extracted values to CSV
         # make sure you output as many values as columns put in the header of the empty CSV above
-        echo "${MONTH},${YEAR},${COUNT_APPLICANTS_INTERNATIONAL_PROTECTION},${TOP_10_NATIONALITIES_APPLICANTS_INTERNATIONAL_PROTECTION},${COUNT_CGVS_DECISIONS}" >> "$OUTPUT_FILE"
+        echo "${MONTH},${YEAR},${COUNT_APPLICANTS_INTERNATIONAL_PROTECTION},${TOP_10_NATIONALITIES_APPLICANTS_INTERNATIONAL_PROTECTION},${COUNT_CGVS_DECISIONS},${WORKLOAD}" >> "$OUTPUT_FILE"
     done
 done
